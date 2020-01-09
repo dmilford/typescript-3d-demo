1f633a698ae5dfed7256da971bad28b6dc87abe0
Doug Milford <milford_doug@yahoo.com>
2020-01-08 21:07:00 -0800

    Prepping to fix normal rotations

--- src/common_funcs.ts
+++ src/common_funcs.ts
@@ -193,25 +193,22 @@ export function projection_and_rotation_normal_for_3d_in_2d_layout(
 ): ProjectionAndRotationNormal {
     const use_scale = 2 * (top - bottom) / canvas_height;
     const aspect = canvas_width / canvas_height;
-    let matrix = perspective(FIELD_OF_VIEW, aspect, Z_NEAR, Z_FAR);
+    const perspective_matrix = perspective(FIELD_OF_VIEW, aspect, Z_NEAR, Z_FAR);
 
-    matrix = translate(matrix, 
+    let modelViewMatrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+    modelViewMatrix = translate(modelViewMatrix, 
         -aspect + (use_scale / 2) + aspect * 2 * (left / canvas_width),
         -1 + (use_scale / 2) + 2 * (bottom / canvas_height),
         Z_PLANE,
     );
-    matrix = xRotate(matrix, rotation_angle_x_axis);
-    matrix = yRotate(matrix, rotation_angle_y_axis);
-    matrix = scale(matrix, use_scale ,use_scale, use_scale);
-    matrix = translate(matrix, -0.5, -0.5, -0.5);
-
-    const normalMatrix = mat4.create();
-    mat4.invert(normalMatrix, normalMatrix);
-    mat4.transpose(normalMatrix, normalMatrix);
+    modelViewMatrix = xRotate(modelViewMatrix, rotation_angle_x_axis);
+    modelViewMatrix = yRotate(modelViewMatrix, rotation_angle_y_axis);
+    modelViewMatrix = scale(modelViewMatrix, use_scale ,use_scale, use_scale);
+    modelViewMatrix = translate(modelViewMatrix, -0.5, -0.5, -0.5);
 
     return { 
-        projection: matrix,
-        rotationNormal: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
+        projection: multiply(perspective_matrix, modelViewMatrix),
+        rotationNormal: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]// transpose(inverse_4x4(modelViewMatrix))
     };
 }
 
@@ -364,3 +361,73 @@ export function zRotate(m: number[], angleInRadians: number): number[] {
 export function scale(m: number[], sx: number, sy: number, sz: number): number[] {
     return multiply(m, scaling(sx, sy, sz));
 }
+
+function inverse_4x4(m: number[]): number[] {
+    const dst = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const m00 = m[0 * 4 + 0];
+    const m01 = m[0 * 4 + 1];
+    const m02 = m[0 * 4 + 2];
+    const m03 = m[0 * 4 + 3];
+    const m10 = m[1 * 4 + 0];
+    const m11 = m[1 * 4 + 1];
+    const m12 = m[1 * 4 + 2];
+    const m13 = m[1 * 4 + 3];
+    const m20 = m[2 * 4 + 0];
+    const m21 = m[2 * 4 + 1];
+    const m22 = m[2 * 4 + 2];
+    const m23 = m[2 * 4 + 3];
+    const m30 = m[3 * 4 + 0];
+    const m31 = m[3 * 4 + 1];
+    const m32 = m[3 * 4 + 2];
+    const m33 = m[3 * 4 + 3];
+    const tmp_0  = m22 * m33;
+    const tmp_1  = m32 * m23;
+    const tmp_2  = m12 * m33;
+    const tmp_3  = m32 * m13;
+    const tmp_4  = m12 * m23;
+    const tmp_5  = m22 * m13;
+    const tmp_6  = m02 * m33;
+    const tmp_7  = m32 * m03;
+    const tmp_8  = m02 * m23;
+    const tmp_9  = m22 * m03;
+    const tmp_10 = m02 * m13;
+    const tmp_11 = m12 * m03;
+    const tmp_12 = m20 * m31;
+    const tmp_13 = m30 * m21;
+    const tmp_14 = m10 * m31;
+    const tmp_15 = m30 * m11;
+    const tmp_16 = m10 * m21;
+    const tmp_17 = m20 * m11;
+    const tmp_18 = m00 * m31;
+    const tmp_19 = m30 * m01;
+    const tmp_20 = m00 * m21;
+    const tmp_21 = m20 * m01;
+    const tmp_22 = m00 * m11;
+    const tmp_23 = m10 * m01;
+
+    const t0 = (tmp_0 * m11 + tmp_3 * m21 + tmp_4 * m31) - (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
+    const t1 = (tmp_1 * m01 + tmp_6 * m21 + tmp_9 * m31) - (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
+    const t2 = (tmp_2 * m01 + tmp_7 * m11 + tmp_10 * m31) - (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
+    const t3 = (tmp_5 * m01 + tmp_8 * m11 + tmp_11 * m21) - (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+
+    const d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
+
+    dst[0] = d * t0;
+    dst[1] = d * t1;
+    dst[2] = d * t2;
+    dst[3] = d * t3;
+    dst[4] = d * ((tmp_1 * m10 + tmp_2 * m20 + tmp_5 * m30) - (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30));
+    dst[5] = d * ((tmp_0 * m00 + tmp_7 * m20 + tmp_8 * m30) - (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30));
+    dst[6] = d * ((tmp_3 * m00 + tmp_6 * m10 + tmp_11 * m30) - (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30));
+    dst[7] = d * ((tmp_4 * m00 + tmp_9 * m10 + tmp_10 * m20) - (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20));
+    dst[8] = d * ((tmp_12 * m13 + tmp_15 * m23 + tmp_16 * m33) - (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33));
+    dst[9] = d * ((tmp_13 * m03 + tmp_18 * m23 + tmp_21 * m33) - (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33));
+    dst[10] = d * ((tmp_14 * m03 + tmp_19 * m13 + tmp_22 * m33) - (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33));
+    dst[11] = d * ((tmp_17 * m03 + tmp_20 * m13 + tmp_23 * m23) - (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23));
+    dst[12] = d * ((tmp_14 * m22 + tmp_17 * m32 + tmp_13 * m12) - (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22));
+    dst[13] = d * ((tmp_20 * m32 + tmp_12 * m02 + tmp_19 * m22) - (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02));
+    dst[14] = d * ((tmp_18 * m12 + tmp_23 * m32 + tmp_15 * m02) - (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12));
+    dst[15] = d * ((tmp_22 * m22 + tmp_16 * m02 + tmp_21 * m12) - (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02));
+
+    return dst;
+  }
